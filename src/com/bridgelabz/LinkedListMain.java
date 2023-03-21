c25ff829c9a01d15c714e50b8dac464d7f8cce45
AnjaliRawke <anjalirawke21@gmail.com>
2023-03-21 18:31:38 +0530

    Added: UC4 Ability to insert 30 between 56 & 70

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -4,10 +4,15 @@ public class LinkedListMain {
 	public static void main(String[] args) {
 		LinkedList<Integer> linkedList1 = new LinkedList<>();
 		linkedList1.append(56);
-		linkedList1.append(30);
 		linkedList1.append(70);
 
+		boolean isInserted = linkedList1.insertAfter(56, 30);
+		if (isInserted)
+			System.out.println("Element inserted successfully!");
+		else
+			System.out.println("Element not inserted!");
 		linkedList1.show();
+
 
 		}
 	}
