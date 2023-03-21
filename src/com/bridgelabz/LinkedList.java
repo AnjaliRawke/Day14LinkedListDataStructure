476a51fa972b72421cf1909a6c0bb39a47fd1d77
AnjaliRawke <anjalirawke21@gmail.com>
2023-03-21 18:21:21 +0530

    Added: UC2 Add Using Push Operation

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -5,14 +5,24 @@ public class LinkedList<T> {
 	Node<T> head;
 	Node<T> tail;
 
-	public void add(T data) {
+	public void push(T data) {
 		Node<T> newNode = new Node(data);
 		if (head == null) {
 			head = newNode;
 			tail = newNode;
 		} else {
-			tail.next = newNode;
-			tail = newNode;
+			newNode.next = head;
+			head = newNode;
 		}
 	}
+
+	public void show() {
+		Node<T> temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + " -> ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+
 }
